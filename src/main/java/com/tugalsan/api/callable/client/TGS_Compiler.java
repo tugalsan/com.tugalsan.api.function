080bbd59d1daa0dd08c44007d85bd7ed147566cd
tugalsan karabacak <tugalsan@gmail.com>
2023-04-17 14:05:27 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/callable/client/TGS_Compiler.java
@@ -0,0 +1,6 @@
+package com.tugalsan.api.callable.client;
+
+public interface TGS_Callable<R> {
+
+    public R call();
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.tugalsan.api.callable {
+    exports com.tugalsan.api.callable.client;
+}
