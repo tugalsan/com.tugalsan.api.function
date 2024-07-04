be4549b7c7068475689704b99119f90df6219a92
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-04 08:06:57 +0300

    .

--- src/main/java/com/tugalsan/api/function/client/TGS_Func_In1.java
+++ src/main/java/com/tugalsan/api/function/client/TGS_Func_In1.java
@@ -1,6 +1,13 @@
 package com.tugalsan.api.function.client;
 
-public interface TGS_Func_In1<A> extends TGS_Func_OutTyped_In1<Void, A> {
+import java.util.function.Consumer;
+
+public interface TGS_Func_In1<A> extends TGS_Func_OutTyped_In1<Void, A>, Consumer<A> {
+
+    @Override
+    default void accept(A input0) {
+        run(input0);
+    }
 
     @Override
     default Void call(A input0) {
--- src/main/java/com/tugalsan/api/function/client/TGS_Func_OutTyped.java
+++ src/main/java/com/tugalsan/api/function/client/TGS_Func_OutTyped.java
@@ -1,8 +1,15 @@
 package com.tugalsan.api.function.client;
 
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-public interface TGS_Func_OutTyped<R> extends Callable<R> {
+public interface TGS_Func_OutTyped<R> extends Callable<R>, Supplier<R> {
+
+    @Override
+    default R get() {
+        return call();
+    }
 
     @Override
     public R call();
