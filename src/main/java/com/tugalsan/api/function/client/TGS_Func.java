bdaadf0806594808e42ab2d74c782948c840839e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-07-23 09:48:55 +0300

    Update TGS_Func.java

--- src/main/java/com/tugalsan/api/function/client/TGS_Func.java
+++ src/main/java/com/tugalsan/api/function/client/TGS_Func.java
@@ -2,6 +2,9 @@ package com.tugalsan.api.function.client;
 
 public interface TGS_Func extends Runnable, TGS_Func_OutTyped<Void> {
 
+    final public static TGS_Func empty = () -> {
+    };
+
     @Override
     default Void call() {
         run();
